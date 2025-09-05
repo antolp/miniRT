df7506deaec60bd6908061b5055389087cae38b9
Antonin Le Page <antoninlepage1@gmail.com>
2025-09-05 20:26:15 +0200

    Fixed fresnel returning faulty weight when toggled off

--- srcs/shading/reflection.c
+++ srcs/shading/reflection.c
@@ -17,8 +17,33 @@
 //thank you mister Schlick
 double fresnel_schlick(double cos_theta, double ior)
 {
-    double r0 = pow((1.0 - ior) / (1.0 + ior), 2.0);
+	double r0;
+    r0 = pow((1.0 - ior) / (1.0 + ior), 2.0);
     return (r0 + (1.0 - r0) * pow(1.0 - cos_theta, 5.0));
+}
+
+//if fresnel toggled on :
+//compute fresnel normally 
+//if fresnel toggled off :
+//flat reflectance, use F0 (Schlick at cosθ = 1, facing surface)
+double	get_fresnel_weight(t_material *m, t_ray *ray, t_hit_info *hit)
+{
+	double	weight;
+	double cos_theta;
+	double F0;
+
+	weight = m->reflectivity;
+	if ((g_renderer(NULL)->shading_flag & FLAG_FRESNEL) != 0u)
+	{
+		cos_theta = fmax(0.0, -vec_dot(ray->direction, hit->normal));
+		weight = weight * fresnel_schlick(cos_theta, m->ior);
+	}
+	else
+	{
+		F0 = fresnel_schlick(1.0, m->ior);
+		weight = weight * F0;
+	}
+	return (fmax(0.0, fmin(1.0, weight)));
 }
 
 //took reflection from old codebase
@@ -41,13 +66,7 @@ t_color	compute_reflection(t_material *m, t_ray *ray, t_hit_info *hit,
 	reflect_dir = vec_normalize(vec_reflect(ray->direction, hit->normal));
 	reflect_ray.origin = vec_add(hit->hit_point, vec_mul(reflect_dir, 0.001));
 	reflect_ray.direction = reflect_dir;
-	weight = m->reflectivity;
-	if ((g_renderer(NULL)->shading_flag & FLAG_FRESNEL) != 0u)
-	{
-		cos_theta = fmax(0.0, -vec_dot(ray->direction, hit->normal));
-		weight = weight * fresnel_schlick(cos_theta, m->ior);
-	}
-	weight = fmax(0.0, fmin(1.0, weight));
+	weight = get_fresnel_weight(m, ray, hit);
 	reflected = trace_ray(&reflect_ray, depth - 1);
 	return (color_lerp(current, reflected, weight));
 }
